f4d16083046446374f5e46cde54cf05b975b5a24
Nguyễn Quyết Thắng <62535477+TieuLang@users.noreply.github.com>
2021-01-21 16:27:33 +0700

    Add files via upload

--- TaoTest.py
+++ TaoTest.py
@@ -11,13 +11,13 @@ for i in range(1,1001):
     s=int(0)
     s1=int(0)
     for j in range(i):
-        a.append(random.randint(2,100))
+        a.append(random.randint(2,10e9))
         b.append(random.randint(1,a[j]-1))
         if (j>0):
             c.append(random.randint(1,j))
         s+=a[j]
         s1+=(a[j]-b[j])
-    w=random.randint(1,min(s1,int(300)))
+    w=random.randint(1,min(s1,int(10e9)))
     file.write("{} {}\n".format(i,w))
     file.write("{} {}\n".format(a[0],b[0]))
     for j in range(1,i):
