a47e4d9935365d85c1fa6e2a6366a8644b99e358
Nguyễn Quyết Thắng <62535477+TieuLang@users.noreply.github.com>
2021-02-01 20:49:49 +0700

    Update Code_de_quy.py

--- Code_de_quy.py
+++ Code_de_quy.py
@@ -1,13 +1,10 @@
-file=open("Test_nho_inp11.txt","r")
-fout=open("out.txt","w")
-
-n,w=map(int,file.readline().split())
+n,w=map(int,input().split())
 
 a=list([])
 b=list([])
 c=list([])
 
-tmp1,tmp2=map(int,file.readline().split())
+tmp1,tmp2=map(int,input().split())
 
 a.append(tmp1)
 b.append(tmp2)
@@ -15,26 +12,31 @@ b[0]=a[0]-b[0]
 c.append(int(-1))
 
 for i in range(n-1):
-    tmp1,tmp2,tmp3=map(int,file.readline().split())
+    tmp1,tmp2,tmp3=map(int,input().split())
     tmp2=tmp1-tmp2
     a.append(tmp1)
     b.append(tmp2)
     c.append(tmp3-1)
 
-kt=list([])
+kt=list([]) #Mang danh dau nhung mon do da mua
+ktt=list([]) #Mang danh dau nhung mon do da mua co dung phieu giam gia
 for i in range(n):
     kt.append(bool(True))
+    ktt.append(bool(True))
 
-ans=int(0)
-ans1=list([])
+ans=int(0) #So mon do nhieu nhat co the mua duoc
+ans1=list([]) #Mang luu nhung mon do da mua theo ket qua
+
 for i in range(n):
     ans1.append(int(0))
+
 def dequy(i,s,tr):
     global ans
     for j in range(tr+1,n):
-        if (kt[j] and (s+a[j]<=w or (s+b[j]<=w and (c[j]==-1 or not kt[c[j]])))):
-            if (c[j]==-1 or not kt[c[j]]):
+        if (kt[j] and (s+a[j]<=w or (s+b[j]<=w and (c[j]==-1 or not ktt[c[j]])))):
+            if (c[j]==-1 or not ktt[c[j]]):
                 s=s+b[j]
+                ktt[j]=False
             else:
                 s=s+a[j]
             kt[j]=False
@@ -46,8 +48,9 @@ def dequy(i,s,tr):
                     if (not kt[z]):
                         ans1[dem]=z
                         dem+=1
-            if (c[j] == -1 or not kt[c[j]]):
+            if (c[j] == -1 or not ktt[c[j]]):
                 s = s - b[j]
+                ktt[j]=True
             else:
                 s = s - a[j]
             kt[j]=True
